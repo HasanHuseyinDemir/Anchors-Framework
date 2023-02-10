46b43d5f0824092876a644e6ebc99b0a6f543846
trojan565 <majorexo565@gmail.com>
2023-02-10 17:00:45 +0300

    .

---
+++ Versions/1.4.6 ForUpdate,Bugfix/betterFor/index.js
@@ -0,0 +1,44 @@
+import {html,For,RegisterComponent} from "./anchors.mjs"
+
+let array=["diffed","Not Diffed#2"];
+
+let Test=()=>{
+    return html`<h1>Test Çalışıyor</h1>`
+}
+
+
+
+//let forList=For(array,(e)=>/*html*/`<p ${e=="text"?"class='text'":""}>${e} ${e=="test"?"<Test/>":""} <span>Not Diffed</span></p>`)
+
+const Page=()=>{
+    let Main=html/*jsx*/`
+    <>
+        <h1>For Test</h1>
+        <Test/>
+        <input $oninput="changeFirst">
+        
+
+        ${For(array,(e)=>/*html*/`
+        <p ${e=="text"?"class='text'":""}>${e} ${e=="test"?"<Test/>":""} <span>Not Diffed</span></p>
+        `)}
+        
+        <style>
+            .text{
+                background-color:gray;
+            }
+        </style>
+    </>
+    `
+
+    Main.states({
+        changeFirst(e){
+            array[0]=e.value;
+        }
+    })
+    return Main
+}
+
+RegisterComponent(Test)
+
+
+app.render(Page)
