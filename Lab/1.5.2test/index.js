2fe569c8141d4ecd57642011d1a4bb3d242b6db4
trojan565 <majorexo565@gmail.com>
2023-04-12 03:28:55 +0300

    .

--- Lab/1.5.2test/index.js
+++ Lab/1.5.2test/index.js
@@ -24,6 +24,16 @@
 
 app.render(new Merhaba().content)*/
 
+class Button{
+    constructor(content,event) {
+        let button=document.createElement("button")
+        button.textContent=content;
+        button.onclick=event
+        return button
+    }
+}
+
+
 class Component{
     constructor(){
         this.template.states({
@@ -33,11 +43,19 @@ class Component{
             }
         })
     }
-    x=0;
+    x=0
     template= html`
     <>
         <h1>Hello</h1> 
         <button $onclick="increase">Increase {{y}}</button>
+        <h1>x=${()=>this.x}</h1>
+        <h1>y={{y}}</h1>
+        ${new Button("Increase",()=>{this.x++,this.template.update()})}
     </>`
 }
+
+console.log(new Component())
 app.render(new Component().template)
+
+
+
