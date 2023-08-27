80dda809119c9704e0d43cf21841d451dd1ee02f
trojan565 <majorexo565@gmail.com>
2023-08-28 00:24:50 +0300

    fix

--- Variants/Vanilla/anchors-vanilla.js
+++ Variants/Vanilla/anchors-vanilla.js
@@ -1,20 +1,22 @@
-function html(data, ...keys) {
-    let elements = [];
-    let keyList = [...keys];
-    data.forEach((item, index) => {
+function html(data,...keys){
+    let elements=[];
+    let keyList=[...keys];
+    data.forEach((item,index)=>{
         elements.push(item);
-        keyList[index] !== undefined?elements.push(keyList[index]):""
-    });
-    let str = "";
-    elements.forEach((item) => {
-        typeof item == "string" ? str += item : str += `<q></q>`;
-    });
-    let el = document.createRange().createContextualFragment(str);
-    [...keys].forEach((key) => {el.querySelector("q").replaceWith((typeof key == "object" || typeof key == "number")?key:typeof key=="function"?key():"")});
-    return el;
+        keyList[index]!=undefined?elements.push(keyList[index]):""
+    })
+    let str="";
+    elements.forEach((item)=>{typeof item=="string"?str+=item:str+=`<xx></xx>`});
+    let el=document.createRange().createContextualFragment(str);
+    [...keys].forEach((key)=>{
+        let t=el.querySelector("xx");
+        (typeof key=="object"||typeof key=="number")?t.replaceWith(key):typeof key=="function"?t.replaceWith(key()):0
+        return
+    })
+    return el
 }
 
-Object.prototype.render = function (s) {
-    this.textContent = "";
-    this.append(typeof s == "object"?s:typeof s=="function"?s():"");
-};
+Object.prototype.render=function(s){
+    this.textContent="";
+    this.append(typeof s=="object"?s:typeof s=="function"?s():"")
+}
